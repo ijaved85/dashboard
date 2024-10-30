cb0a456cf383c36927d4281ba366d28d80244c7a
ijaved85 <afifislam8572@gmail.com>
2024-10-30 16:17:13 +0530

    bug fixed

--- dashboard/Dashboard.js
+++ dashboard/Dashboard.js
@@ -1,41 +1,38 @@
-
 const wallet = () => {
-  $('#customer-form').hide();
-  $('#transaction-form').hide();
-  $('#addEntry').hide();
-  $('#downEntry').hide();
-  $('#tran-info').hide();
-   $mfOperation.hide();
-   $mfEntryLink.hide();
-  activePage('wallet');
+	$("#customer-form").hide();
+	$("#transaction-form").hide();
+	$("#addEntry").hide();
+	$("#downEntry").hide();
+	$("#tran-info").hide();
+	$mfOperation.hide();
+	$mfEntryLink.hide();
+	activePage("wallet");
 
+	let IWP = 0;
+	let IWR = 0;
+	db.ref("Wallet").once("value", snap => {
+		const $walletListContainer = $("#fireWallet");
+		$walletListContainer.html("");
 
-  let IWP = 0;
-  let IWR = 0;
-  db.ref('Wallet').once('value',
-    (snap) => {
-      const $walletListContainer = $('#fireWallet');
-      $walletListContainer.html('');
+		snap.forEach(fire => {
+			const key = fire.key;
+			const data = fire.val();
+			const name = data.Name;
+			const photo = data.Photo;
+			const total = data.Total;
 
-      snap.forEach((fire) => {
-        const key = fire.key;
-        const data = fire.val();
-        const name = data.Name;
-        const photo = data.Photo;
-        const total = data.Total;
-
-        let state = '';
-        let walletHTML = '';
-        if (total >= 0) {
-          IWP += total;
-          state = 'pay';
-        } else {
-          IWR += total
-          state = 'receive';
-        }
-        $('.Receive').html(parseFloat(IWR) * -1);
-        $('.Pay').html(IWP);
-        walletHTML += `
+			let state = "";
+			let walletHTML = "";
+			if (total >= 0) {
+				IWP += total;
+				state = "pay";
+			} else {
+				IWR += total;
+				state = "receive";
+			}
+			$(".Receive").html(parseFloat(IWR) * -1);
+			$(".Pay").html(IWP);
+			walletHTML += `
         <tr data-key="${key}" onclick="walletTrans(${key})" ondblclick="oper(${key})">
         <td style="justify-content: center">
         <img src="${photo}" ></td><td>
@@ -45,55 +42,53 @@ const wallet = () => {
         <span class="${state}">${total}</span>
         </td>
         </tr>`;
-        $walletListContainer.append(walletHTML);
-      });
-    });
-}
+			$walletListContainer.append(walletHTML);
+		});
+	});
+};
 
-const walletTrans = (key) => {
-  $('#addEntry').show();
-  $('#downEntry').show();
- 
-  
-  const $walletRef = db.ref(`Wallet/${key}/Data`);
-  const $nameRef = db.ref(`Wallet/${key}/`);
-  const $transHisContainer = $('#transHis');
-  $('#addEntry').attr('key', key);
-  $('#Entry').attr('key', key);
+const walletTrans = key => {
+	$("#addEntry").show();
+	$("#downEntry").show();
 
+	const $walletRef = db.ref(`Wallet/${key}/Data`);
+	const $nameRef = db.ref(`Wallet/${key}/`);
+	const $transHisContainer = $("#transHis");
+	$("#addEntry").attr("key", key);
+	$("#Entry").attr("key", key);
 
-  $nameRef.once('value', snap => {
-   let data = snap.val();
-    let name = data.Name;
-    let total = data.Total;
-    $("#custName").html(name);
-  });
-  $("#custPhone").html(key);
+	$nameRef.once("value", snap => {
+		let data = snap.val();
+		let name = data.Name;
+		let total = data.Total;
+		$("#custName").html(name);
+	});
+	$("#custPhone").html(key);
 
-  let walletHTML = '';
-  $walletRef.on('value', (snap) => {
-    const data = snap.val();
+	let walletHTML = "";
+	$walletRef.on("value", snap => {
+		const data = snap.val();
 
-    // Convert data to an array and sort it by SINo in descending order
-    const sortedData = Object.entries(data).sort((a, b) => {
-      const sinA = a[1].SINo;
-      const sinB = b[1].SINo;
-      return sinB - sinA;
-    });
+		// Convert data to an array and sort it by SINo in descending order
+		const sortedData = Object.entries(data).sort((a, b) => {
+			const sinA = a[1].SINo;
+			const sinB = b[1].SINo;
+			return sinB - sinA;
+		});
 
-    sortedData.forEach(([key, id]) => {
-      let state = '';
-      if (id.Pay == 0) {
-        state = 'pay';
-      } else {
-        state = 'receive';
-      }
+		sortedData.forEach(([key, id]) => {
+			let state = "";
+			if (id.Pay == 0) {
+				state = "pay";
+			} else {
+				state = "receive";
+			}
 
-      const date = new Date(id.Date);
-      const options = { day: 'numeric', month: 'short', year: 'numeric' };
-      const formattedDate = date.toLocaleDateString('en-GB', options);
+			const date = new Date(id.Date);
+			const options = { day: "numeric", month: "short", year: "numeric" };
+			const formattedDate = date.toLocaleDateString("en-GB", options);
 
-      walletHTML += `
+			walletHTML += `
       <tr data-key="${id}" id="transRow">
       <td>
       <p class="date">${formattedDate}</p>
@@ -101,42 +96,43 @@ const walletTrans = (key) => {
       <td><span class="purpose">${id.Purpose}</span></td>
       <td><span class="${state}">${id.Pay || id.Receive}</span></td>
       </tr>`;
-    });
-    $transHisContainer.html(walletHTML);
-     $('#tran-info').show();
-  });
-}
+		});
+		$transHisContainer.html(walletHTML);
+		$("#tran-info").show();
+	});
+};
 
 const market = () => {
-  activePage('market');
-  wallet();
-   $('#mutualform').hide();
-}
+	activePage("market");
+	wallet();
+	$("#mutualform").hide();
+};
 
 const cards = () => {
-  activePage("cards");
-  db.ref('Cards').once('value', (snap) => {
-    const $cardsContainer = $('#fireCards');
+	activePage("cards");
+	db.ref("Cards").once("value", snap => {
+		const $cardsContainer = $("#fireCards");
 
-    snap.forEach((fire) => {
-      const cardName = fire.key;
-      const Data = fire.val();
-      const cvv = Data.CVV;
-      const cardHolder = Data.CardHolder;
-      const bank = Data.Bank;
-      const cardType = Data.CardType;
-      const company = Data.CardCompany;
-      const expDate = Data.ExpiryDate;
-      const note = Data.Note;
-      const colour = Data.Background;
-      const cardNumber = [
-        Data.CardNumber.First,
-        Data.CardNumber.Second,
-        Data.CardNumber.Third,
-        Data.CardNumber.Fourth
-      ];
-      const fullNumber = cardNumber[0]+cardNumber[1]+cardNumber[2]+cardNumber[3];
-      const cardHTML = `<div class="card" onclick="flipCard('${fullNumber}')">
+		snap.forEach(fire => {
+			const cardName = fire.key;
+			const Data = fire.val();
+			const cvv = Data.CVV;
+			const cardHolder = Data.CardHolder;
+			const bank = Data.Bank;
+			const cardType = Data.CardType;
+			const company = Data.CardCompany;
+			const expDate = Data.ExpiryDate;
+			const note = Data.Note;
+			const colour = Data.Background;
+			const cardNumber = [
+				Data.CardNumber.First,
+				Data.CardNumber.Second,
+				Data.CardNumber.Third,
+				Data.CardNumber.Fourth
+			];
+			const fullNumber =
+				cardNumber[0] + cardNumber[1] + cardNumber[2] + cardNumber[3];
+			const cardHTML = `<div class="card" onclick="flipCard('${fullNumber}')">
       <div class="card-inner" id="${fullNumber}card">
       <div class="front" style="background:${colour}" id="${fullNumber}front">
       <img src="${Data.Logo}" class="back-img">
@@ -186,20 +182,38 @@ const cards = () => {
       </div>
       </div>
       `;
-      $cardsContainer.append(cardHTML);
-    });
-  });
-  market();
-}
+			$cardsContainer.append(cardHTML);
+		});
+	});
+	market();
+};
 
-const qrcode = () => {
-  activePage('qrcode');
-  cards();
-}
+let clickCount = {};
+const flipCard = cardNo => {
+	if (!clickCount[cardNo]) {
+		clickCount[cardNo] = 0;
+	}
+	clickCount[cardNo]++;
+	if (clickCount[cardNo] === 5) {
+		$(`#${cardNo}card`).addClass("flipped");
+		clickCount[cardNo] = 0;
+	} else {
+		$(`#${cardNo}card`).removeClass("flipped");
+	}
+	// Reset counter after 2 seconds
+	setTimeout(() => {
+		clickCount[cardNo] = 0;
+	}, 1000);
+};
 
 const aku = () => {
-  activePage('aku');
-  qrcode();
+	activePage("aku");
+	cards();
+};
+
+/*const qrcode = () => {
+  activePage('qrcode');
+  cards();
 }
 
 const members = () => {
@@ -233,24 +247,6 @@ const members = () => {
 //  $("#preloader").fadeOut(4500);
 }
 
-let clickCount = {};
-const flipCard = (cardNo) => {
-  if (!clickCount[cardNo]) {
-    clickCount[cardNo] = 0;
-  }
-  clickCount[cardNo]++;
-  if (clickCount[cardNo] === 5) {
-    $(`#${cardNo}card`).addClass('flipped');
-    clickCount[cardNo] = 0;
-  } else {
-    $(`#${cardNo}card`).removeClass('flipped');
-  }
-  // Reset counter after 2 seconds
-  setTimeout(() => {
-    clickCount[cardNo] = 0;
-  }, 1000);
-}
-
 const gemini = () => {
   activePage('gemini');
   members();
@@ -259,16 +255,14 @@ const gemini = () => {
 const notes = () => {
   activePage('note');
   gemini();
-}
-
-$(document).ready(async function() {
-  await loadPageContent();
-  setTimeout(function() {
-      $("#preloader").fadeOut(900);
-  }, 9000);
-
+}*/
+$(document).ready(async function () {
+	await loadPageContent();
+	setTimeout(function () {
+		$("#preloader").fadeOut(900);
+	}, 9000);
 });
 
 async function loadPageContent() {
-  notes();
+	aku();
 }
--- dashboard/Market/Scriptfiles/common.js
+++ dashboard/Market/Scriptfiles/common.js
@@ -1,217 +1,217 @@
 /***********************Dark Theme*****************************/
-const $modeLink = $('#switch-mode');
-const $faSpan = $modeLink.find('.fa');
-const $textSpan = $modeLink.find('.text');
+const $modeLink = $("#switch-mode");
+const $faSpan = $modeLink.find(".fa");
+const $textSpan = $modeLink.find(".text");
 
 // Detect system dark mode preference
-const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)');
+const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
 if (prefersDarkMode.matches) {
-  // System is in dark mode, set initial state accordingly
-  $modeLink.addClass('active');
-  $faSpan.text('light_mode');
-  $textSpan.text('Light Mode');
-  $('body').addClass('dark');
-   $('#aku').contents().find('iframe').css('filter', 'invert(100%)');
-   localStorage.setItem("theme", "dark");
+	// System is in dark mode, set initial state accordingly
+	$modeLink.addClass("active");
+	$faSpan.text("light_mode");
+	$textSpan.text("Light Mode");
+	$("body").addClass("dark");
+	$("#mutualfund").contents().find("body").removeClass("darks");
+	$("#mutualform").contents().find("body").removeClass("darks");
 } else {
-  // System is in light mode, set initial state accordingly
-  $modeLink.removeClass('active');
-  $faSpan.text('dark_mode');
-  $textSpan.text('Dark Mode');
-  $('body').removeClass('dark');
-   $('#aku').contents().find('iframe').css('filter', 'invert(0%)');
-   localStorage.setItem("theme", "light");
+	// System is in light mode, set initial state accordingly
+	$modeLink.removeClass("active");
+	$faSpan.text("dark_mode");
+	$textSpan.text("Dark Mode");
+	$("body").removeClass("dark");
+	$("#mutualfund").contents().find("body").addClass("darks");
+	$("#mutualform").contents().find("body").addClass("darks");
 }
 
 // Add event listener to toggle button
-$modeLink.on('click', function() {
-  if ($(this).hasClass('active')) {
-    $(this).removeClass('active');
-    $faSpan.text('dark_mode');
-    $textSpan.text('Dark Mode');
-    $('body').removeClass('dark');
-    $('#notes').contents().find('body').removeClass('dark'); 
-    $('#mutualfund').contents().find('body').removeClass('dark'); 
-    $('#mutualform').contents().find('body').removeClass('dark'); 
-    $('#gemini').contents().find('body').addClass('light_mode'); 
-    $('#aku').contents().find('body').addClass('light_mode'); 
-    $('#qrcode').contents().find('body').addClass('light_mode'); 
-    $('#aku').contents().find('iframe').css('filter', 'invert(0%)');
-    localStorage.setItem("theme", "light");
-  } else {
-    $(this).addClass('active');
-    $faSpan.text('light_mode');
-    $textSpan.text('Light Mode');
-    $('body').addClass('dark');
-    $('#notes').contents().find('body').addClass('dark');
-    $('#mutualfund').contents().find('body').addClass('dark');
-    $('#mutualform').contents().find('body').addClass('dark');
-    $('#gemini').contents().find('body').removeClass('light_mode'); 
-    $('#qrcode').contents().find('body').removeClass('light_mode'); 
-    $('#aku').contents().find('body').removeClass('light_mode'); 
-    $('#aku').contents().find('iframe').css('filter', 'invert(100%)');
-   localStorage.setItem("theme", "dark");
-  }
+$modeLink.on("click", function () {
+	if ($(this).hasClass("active")) {
+		$(this).removeClass("active");
+		$faSpan.text("dark_mode");
+		$textSpan.text("Dark Mode");
+		$("body").removeClass("dark");
+		$("#notes").contents().find("body").removeClass("dark");
+		$("#mutualfund").contents().find("body").removeClass("darks");
+		$("#mutualform").contents().find("body").removeClass("darks");
+		$("#gemini").contents().find("body").addClass("light_mode");
+		$("#aku").contents().find("body").addClass("light_mode");
+		$("#qrcode").contents().find("body").addClass("light_mode");
+		$("#aku").contents().find("iframe").css("filter", "invert(0%)");
+		localStorage.setItem("theme", "light");
+	} else {
+		$(this).addClass("active");
+		$faSpan.text("light_mode");
+		$textSpan.text("Light Mode");
+		$("body").addClass("dark");
+		$("#notes").contents().find("body").addClass("dark");
+		$("#mutualfund").contents().find("body").addClass("darks");
+		$("#mutualform").contents().find("body").addClass("darks");
+		$("#gemini").contents().find("body").removeClass("light_mode");
+		$("#qrcode").contents().find("body").removeClass("light_mode");
+		$("#aku").contents().find("body").removeClass("light_mode");
+		$("#aku").contents().find("iframe").css("filter", "invert(100%)");
+		localStorage.setItem("theme", "dark");
+	}
 });
 /**************************Dark Theme**************************/
 
-const formatDate = (dateString) => {
-  const date = new Date(dateString);
-  const options = {
-    day: 'numeric', month: 'short', year: 'numeric'
-  };
-  const formattedDate = date.toLocaleDateString('en-GB',
-    options);
+const formatDate = dateString => {
+	const date = new Date(dateString);
+	const options = {
+		day: "numeric",
+		month: "short",
+		year: "numeric"
+	};
+	const formattedDate = date.toLocaleDateString("en-GB", options);
 
-  return formattedDate;
+	return formattedDate;
 };
 
+const cusStatement = key => {
+	$(".downPre").fadeIn();
+	const iframe = $("#invoice")[0]; // Get the iframe element
+	const iframeDoc = iframe.contentWindow.document; // Get the iframe's document object
 
+	const $detailRef = db.ref(`Wallet/${key}/Data`).orderByChild("Date");
 
-const cusStatement = (key) => {
-  $(".downPre").fadeIn();
-  const iframe = $('#invoice')[0]; // Get the iframe element
-  const iframeDoc = iframe.contentWindow.document; // Get the iframe's document object
- 
-  const $detailRef = db.ref(`Wallet/${key}/Data`).orderByChild('Date');
+	let walletRef = db.ref(`Wallet/${key}/`);
+	walletRef.once("value", snap => {
+		data = snap.val();
+		name = data.Name;
+	});
 
-  let walletRef = db.ref(`Wallet/${key}/`);
-  walletRef.once('value',
-    snap => {
-      data = snap.val();
-      name = data.Name;
-    });
+	console.log(name);
+	$(iframeDoc).find(".StaName").html(name);
+	$(iframeDoc).find(".StaNumber").html(key);
+	let fromDate = $("#fromDate").val();
+	let toDate = $("#toDate").val();
 
-  console.log(name);
-  $(iframeDoc).find('.StaName').html(name);
-  $(iframeDoc).find('.StaNumber').html(key);
-  let fromDate = $('#fromDate').val();
-  let toDate = $('#toDate').val();
-
-  if (fromDate === "" || toDate === "") {
-     $(iframeDoc).find('.Dater').html('All Transactions');
-    // Retrieve all transactions
-    $detailRef.once('value', (snap) => {
-      // Process the data
-      processTransactions(snap, key);
-    });
-  } else {
-     $(iframeDoc).find('.Dater').show();
-     let formattedDate1 = formatDate(fromDate);
-  let formattedDate2 = formatDate(toDate);
-  $(iframeDoc).find('.fromD').html(formattedDate1);
-    $(iframeDoc).find('.toD').html(formattedDate2);
-    // Use the provided dates to filter the transactions
-    $detailRef.startAt(fromDate).endAt(toDate).once('value', (snap) => {
-      // Process the data
-      processTransactions(snap, key);
-    });
-  }
-}
+	if (fromDate === "" || toDate === "") {
+		$(iframeDoc).find(".Dater").html("All Transactions");
+		// Retrieve all transactions
+		$detailRef.once("value", snap => {
+			// Process the data
+			processTransactions(snap, key);
+		});
+	} else {
+		$(iframeDoc).find(".Dater").show();
+		let formattedDate1 = formatDate(fromDate);
+		let formattedDate2 = formatDate(toDate);
+		$(iframeDoc).find(".fromD").html(formattedDate1);
+		$(iframeDoc).find(".toD").html(formattedDate2);
+		// Use the provided dates to filter the transactions
+		$detailRef
+			.startAt(fromDate)
+			.endAt(toDate)
+			.once("value", snap => {
+				// Process the data
+				processTransactions(snap, key);
+			});
+	}
+};
 
 const processTransactions = (snap, key) => {
-  const iframe = $('#invoice')[0]; // Get the iframe element
-  const iframeDoc = iframe.contentWindow.document; // Get the iframe's document object
+	const iframe = $("#invoice")[0]; // Get the iframe element
+	const iframeDoc = iframe.contentWindow.document; // Get the iframe's document object
 
-  let date = new Date();
-  let formattedDate = formatDate(date);
-  $(iframeDoc).find('.genDate').html(formattedDate);
+	let date = new Date();
+	let formattedDate = formatDate(date);
+	$(iframeDoc).find(".genDate").html(formattedDate);
 
-  const $fireState = $(iframeDoc).find('#fireStatement');
+	const $fireState = $(iframeDoc).find("#fireStatement");
 
-  $fireState.html('');
+	$fireState.html("");
 
+	let calculate = 0; // Initialize the calculate variable
+	snap.forEach(childSnap => {
+		const id = childSnap.val();
 
-  let calculate = 0; // Initialize the calculate variable
-  snap.forEach((childSnap) => {
-    const id = childSnap.val();
+		const formattedDate = formatDate(id.Date);
 
-    const formattedDate = formatDate(id.Date);
+		if (id.Pay == 0) {
+			calculate += id.Receive;
+		} else {
+			calculate += id.Pay;
+		}
 
-    if (id.Pay == 0) {
-      calculate += id.Receive;
-    } else {
-      calculate += id.Pay;
-    }
-
-    if (id.Balance >= 0) {
-      state = "green";
-    } else {
-      state = "red";
-    }
-    let statementHTML = ` <tr>
+		if (id.Balance >= 0) {
+			state = "green";
+		} else {
+			state = "red";
+		}
+		let statementHTML = ` <tr>
     <td class="date" style="white-space: nowrap;">${formattedDate}</td>
     <td style="text-align:left;">${id.Purpose}</td>
     <td class="withdraw" style="color:red; text-align: right; padding-right: 5px;">${id.Pay}</td>
     <td class="deposit" style="color:green; text-align: right; padding-right:5px;">${id.Receive}</td>
     <td class="balance" style="color:${state}; text-align: right; padding-right:5px;">${id.Balance}</td>
     </tr>`;
-    $fireState.append(statementHTML);
-    $(iframeDoc).find('.total').text(calculate).css('color', state);
-  });
-  downloadPdf();
-}
-
-
-
-
+		$fireState.append(statementHTML);
+		$(iframeDoc).find(".total").text(calculate).css("color", state);
+	});
+	downloadPdf();
+};
 
 const downloadPdf = () => {
-  const iframe = document.getElementById('invoice');
-  const iframeDoc = iframe.contentWindow.document;
-  const iframeContent = iframe.contentDocument || iframe.contentWindow.document;
-  const html = iframeContent.body.innerHTML;
+	const iframe = document.getElementById("invoice");
+	const iframeDoc = iframe.contentWindow.document;
+	const iframeContent = iframe.contentDocument || iframe.contentWindow.document;
+	const html = iframeContent.body.innerHTML;
 
-  let name = $(iframeDoc).find('.StaName').text();
-  let phone = $(iframeDoc).find('.StaNumber').text();
+	let name = $(iframeDoc).find(".StaName").text();
+	let phone = $(iframeDoc).find(".StaNumber").text();
 
-  let upperCaseName = name.substring(0,
-    4).toUpperCase();
-  let lastFourDigitsPhone = phone.substring(phone.length - 4);
+	let upperCaseName = name.substring(0, 4).toUpperCase();
+	let lastFourDigitsPhone = phone.substring(phone.length - 4);
 
-  let password = upperCaseName + lastFourDigitsPhone;
-  let datetime = new Date().toISOString().replace(/:/g, '').replace(/\..+/, '');
-let fname = `${name.toUpperCase()}${datetime}`;
+	let password = upperCaseName + lastFourDigitsPhone;
+	let datetime = new Date().toISOString().replace(/:/g, "").replace(/\..+/, "");
+	let fname = `${name.toUpperCase()}${datetime}`;
 
-  // Add a wrapper div to control margins and padding
-  const wrapperDiv = document.createElement('div');
-  wrapperDiv.style.margin = '0';
-  wrapperDiv.style.padding = '0';
-  wrapperDiv.innerHTML = html;
+	// Add a wrapper div to control margins and padding
+	const wrapperDiv = document.createElement("div");
+	wrapperDiv.style.margin = "0";
+	wrapperDiv.style.padding = "0";
+	wrapperDiv.innerHTML = html;
 
-  const pdf = new html2pdf();
-  pdf.from(wrapperDiv).set({
-    margin: 0.1,
-    // set margin to 0
-    filename: fname,
-    image: {
-      type: 'jpeg',
-      quality: 1
-    },
-    html2canvas: {
-      scale: 2.9,
-      // increase scale to reduce whitespace
-      useCORS: true
-    },
-    jsPDF: {
-      unit: 'in',
-      format: 'A4',
-      orientation: 'portrait'
-    }
-  }).output('blob').then((blob) => {
-    if (!blob) {
-      console.error('Blob is null or undefined');
-      return;
-    }
-    const url = URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    a.href = url;
-    a.download = fname;
-    a.click();
-    URL.revokeObjectURL(url);
-    $(".downPre").fadeOut();
-    $downEntryLink.click();
-  }).catch((err) => {
-    console.error('Error generating PDF:', err);
-  });
-}
+	const pdf = new html2pdf();
+	pdf
+		.from(wrapperDiv)
+		.set({
+			margin: 0.1,
+			// set margin to 0
+			filename: fname,
+			image: {
+				type: "jpeg",
+				quality: 1
+			},
+			html2canvas: {
+				scale: 2.9,
+				// increase scale to reduce whitespace
+				useCORS: true
+			},
+			jsPDF: {
+				unit: "in",
+				format: "A4",
+				orientation: "portrait"
+			}
+		})
+		.output("blob")
+		.then(blob => {
+			if (!blob) {
+				console.error("Blob is null or undefined");
+				return;
+			}
+			const url = URL.createObjectURL(blob);
+			const a = document.createElement("a");
+			a.href = url;
+			a.download = fname;
+			a.click();
+			URL.revokeObjectURL(url);
+			$(".downPre").fadeOut();
+			$downEntryLink.click();
+		})
+		.catch(err => {
+			console.error("Error generating PDF:", err);
+		});
+};
